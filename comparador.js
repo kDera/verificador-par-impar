6ab33b73f164cd5a9d86324b8e3e10e17cd62856
kDera <caduvazquezabj@gmail.com>
2024-02-06 18:19:38 -0300

    terinmar desafio

--- comparador.js
+++ comparador.js
@@ -1,13 +1,15 @@
 
 function parImpar(){
     var resultAtual = parseInt(document.getElementById("resultAtual").value);
+   
 
+    //se onumero for quebrado, mensgem de inválio
     if(resultAtual % 2 === 0){
-        document.getElementById("resultado").innerText = "o numero "+ resultAtual+"é PAR";
+        document.getElementById("resultado").innerText = "o numero "+ resultAtual+" é PAR";
     }
     else{
         document.getElementById("resultado").innerText = "o numero é "+ resultAtual+" IMPAR";
     }
-
+ 
 }
 
