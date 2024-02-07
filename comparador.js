58b0ba21293363bfbf61501ba03ad1d0f315da61
kDera <caduvazquezabj@gmail.com>
2024-02-07 12:50:40 -0300

    sistema funcional

--- comparador.js
+++ comparador.js
@@ -1,15 +1,22 @@
 
+
 function parImpar(){
-    var resultAtual = parseInt(document.getElementById("resultAtual").value);
-   
+    var number = parseFloat(document.getElementById("resultAtual").value);
+    const resultado = document.getElementById("resultado")
 
     //se onumero for quebrado, mensgem de inválio
-    if(resultAtual % 2 === 0){
-        document.getElementById("resultado").innerText = "o numero "+ resultAtual+" é PAR";
+   //console.log()
+    if (Number.isInteger(number)) {
+        if(number% 2=== 0){
+            document.getElementById("resultado").innerText = "o numero "+ number+" é PAR";
+        }
+        else{
+            document.getElementById("resultado").innerText = "o numero "+ number+" é impar";
+        }
+    } else {
+        console.log("INVALIDO O valor inserido é um número decimal.");
+        document.getElementById("resultado").innerText = "o numero "+ number+" é inválido devido ter casa decimal";
     }
-    else{
-        document.getElementById("resultado").innerText = "o numero é "+ resultAtual+" IMPAR";
-    }
- 
+     console.log(Number.isInteger(number))
+    
 }
-
